74becd1debcfe29165216fb738fa895d4dc55359
Angel <3965@holbertonschool.com>
2021-10-01 13:42:02 -0700

    variables c 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - base16
+*Return: 0
+*/
+
+	int main(void)
+	{
+	
+		int n, N;
+
+	for (n = '0'; n <= '9'; n++)
+	putchar(n);
+	for (N = 'a'; N <= 'f'; N++)
+	putchar(N);
+	putchar('\n');
+	return (0);
+}
