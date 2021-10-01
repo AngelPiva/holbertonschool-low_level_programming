2184fa892b59878b117bbca95c52425af91b91ec
Angel <3965@holbertonschool.com>
2021-10-01 11:28:06 -0700

    variables 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - abcABC
+*Return: 0
+*/
+
+	int main(void)
+	{
+		int n, N;
+	for (n = 'a'; n <= 'z'; n++)
+	putchar (n);
+	for (N = 'A'; N <= 'Z'; N++)
+	putchar (N);
+	putchar ('\n');
+	return (0);
+}
