d434828880033447409fc309dace1a2dcb10f923
Angel <3965@holbertonschool.com>
2021-10-01 12:03:14 -0700

    variables c 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - abc-qe
+*Return: 0
+*/
+
+int main(void)
+
+	{
+		int n;
+	for (n = 'a'; n <= 'z'; n++)
+	if (n != 'e' && n != 'q')
+	putchar (n);
+	putchar ('\n');
+	return (0);
+}
