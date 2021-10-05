458726cdfcbe4cb2a0d9567ad3e5fee27ae49eaf
Angel <3965@holbertonschool.com>
2021-10-05 10:10:55 -0700

    functions c2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - alphabet x10
+*Return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int n, r = 0;
+	while (r <= 10)
+	{
+	r++;
+	for (n = 'a'; n <= 'z'; n++)
+	_putchar(n);
+	_putchar('\n');
+	}
+}
