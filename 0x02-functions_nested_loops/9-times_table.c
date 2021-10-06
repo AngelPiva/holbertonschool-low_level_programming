3a5a959356b8d013b034d8cf72dd3dc7d1a8373f
Angel <3965@holbertonschool.com>
2021-10-05 18:34:59 -0700

    functions c9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+*times_table - tablas
+*Return: 0
+*/
+
+void times_table(void)
+{
+	int n1, n2, pr;
+
+	for (n1 = 0; n1 <= 9; n1++)
+	{
+		for (n2 = 0; n2 <= 9; n2++)
+		{
+			pr = n1 * n2;
+			if (n2 != 0)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+			if (n2 == 0)
+			{
+			_putchar('0');
+			}
+			else if (pr >= 10)
+			{
+			_putchar((pr / 10) + '0');
+			_putchar((pr % 10) + '0');
+			}
+			else if ((pr < 10) && (n2 != 0))
+			{
+			_putchar(' ');
+			_putchar((pr % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}	
+}
