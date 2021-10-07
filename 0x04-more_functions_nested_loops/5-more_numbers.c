1cb331e34b919dddb87223dfcd98f3e9a4f6d1ae
Angel <3965@holbertonschool.com>
2021-10-07 10:38:44 -0700

    morefunctions c5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*more_numbers - prints 10 times the numbers, from 0 to 14, followed by a new line
+*
+*/
+
+void more_numbers(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 <= 9; n1++)
+	{
+	for (n2 = 0; n2 <= 14; n2++)
+	{
+	if (n2 >= 10)
+	_putchar((n2 / 10) % 10 + 48);
+	_putchar((n2 % 10) + 48);
+	}
+	_putchar('\n');
+	}
+}
