db77f59af1e45530eb77a02dc44e00ef78b2b906
Angel <3965@holbertonschool.com>
2021-10-08 10:31:57 -0700

    more functions c10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*
+*
+*/
+
+void print_triangle(int size)
+{
+	int a, b, e;
+
+	if (size <= 0)
+		_putchar ('\n');
+	for (a = 1; a <= size; a++)
+	{
+		for (b = size; b > a; b--)
+		{
+			_putchar(' ');
+		}
+		for (e = 1; e <= a; e++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
