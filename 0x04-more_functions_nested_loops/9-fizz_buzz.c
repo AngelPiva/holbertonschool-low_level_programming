00747614546c26183e51afb8191f407db1ec910f
Angel <3965@holbertonschool.com>
2021-10-08 07:27:27 -0700

    more functions c9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,22 +13,22 @@ int main(void)
 	{
 	if (num % 15 == 0)
 	{
-	printf("FizzBuzz ");
+	printf("FizzBuzz");
 	}
 	else if (num % 3 == 0)
 	{
-	printf("Fizz ");
+	printf("Fizz");
 	}
 	else if (num % 5 == 0)
 	{
-	printf("Buzz ");
+	printf("Buzz");
 	}
-	else if (num > 99)
-	printf("%d", num);
 	else
 	{
-	printf("%d ", num);
+	printf("%d", num);
 	}
+	if (num != 100)
+	printf(" ");
 	}
 	putchar('\n');
 	return (0);
