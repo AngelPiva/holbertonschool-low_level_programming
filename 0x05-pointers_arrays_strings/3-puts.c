e2195d286b0973e07a0f366d834f851b37590d69
Angel <3965@holbertonschool.com>
2021-10-12 11:31:26 -0700

    pointers C3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_puts - prints a string
+*@str: pointer
+*/
+
+void _puts(char *str)
+{
+	int c = 0;
+	while(str[c] != '\0')
+	{
+		_putchar(str[c]);
+		c++;
+	}
+	_putchar('\n');
+}
