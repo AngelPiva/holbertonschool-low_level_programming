42bcee8db8934003e84a2b016fd6edbd0d3c2d06
Angel <3965@holbertonschool.com>
2021-10-12 12:09:49 -0700

    pointers C4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*
+*
+*/
+
+void print_rev(char *s)
+{
+	int c = 0;
+	
+	while (s[c] != '\0')
+	{
+	c++;
+	}
+	for (c--; c >= 0; c--)
+	_putchar(s[c]);
+	_putchar('\n');
+}	
