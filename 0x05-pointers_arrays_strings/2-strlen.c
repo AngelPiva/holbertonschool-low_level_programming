1b090cd54d7d7e18c442630c02b13f7d78169b03
Angel <3965@holbertonschool.com>
2021-10-12 10:49:50 -0700

    pointers C2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_strlen - returns the length of a string
+*@s: pointer
+*Return: 0
+*/
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	c++;
+	return (c);
+	return (0);
+}
