19460d3993dae06454e26410e75a4e5d74685bb5
AngelPiva <angelpiva6@gmail.com>
2021-10-13 09:31:55 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*
+*
+*/
+
+void rev_string(char *s)
+{
+	int i;
+	int cont = 0;
+	char f = s[0];
+
+	while (s[cont] != 0)
+		cont++;
+	for (i =0; i < cont; i++)
+	{
+		cont--;
+		f = s[i];
+		s[i] = s[cont];
+		s[cont] = f;
+	}
+}
