c5c9c4f7cc73c9987ef2a558b5774fcd33b1a121
AngelPiva <angelpiva6@gmail.com>
2021-10-15 05:31:49 -0700

    more pointers C2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*
+*
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int c;
+
+	for (c = 0; c < n && src[c] != '\0'; c++)
+		dest[c] = src[c];
+	for ( ; c < n; c++)
+		dest[c] = '\0';
+	return (dest);
+}
