5fec562ac776d5b76ccdf092bf5d6d46c2bb6f4e
AngelPiva <angelpiva6@gmail.com>
2021-10-15 09:20:31 -0700

    more pointers C3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
 *_strcmp - compares two strings
 *@s1: string 1
 *@s2: string 2
-Return: t
+*Return: t
 */
 
 int _strcmp(char *s1, char *s2)
@@ -14,6 +14,6 @@ int _strcmp(char *s1, char *s2)
 	while (s1[r] == s2[r] && s1[r] != '\0' && s2[r] != '\0')
 		r++;
 	if (s1[r] != s2[r])
-		t= s1[r] - s2[r];
-	return(t);
+		t = s1[r] - s2[r];
+	return (t);
 }
