9e7ed5183b5807a2237c34b421f2f0479ee4bff7
AngelPiva <angelpiva6@gmail.com>
2021-10-15 19:17:30 -0700

    more pointers C6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*
+*
+*/
+
+char *cap_string(char *str)
+{
+	int c = 0;
+	int e;
+
+	while (str[c] != '\0')
+	c++;
+
+	for (e = 0; e < c; e++)
+	{
+		if (e == 0 && str[e] >= 97 && str[e] <= 122)
+			str[e] = str[e] - 32;
+		if (str[e] >= 97 && str[e] <= 122)
+		{
+			if (str[e - 1] == ' ' || str[e - 1] == '\t' ||
+				str[e - 1] == '\n' || str[e - 1] == ',' ||
+				str[e - 1] == ';' || str[e - 1] == '.' ||
+				str[e - 1] == '!' || str[e - 1] == '?' ||
+				str[e - 1] == '"' || str[e - 1] == '(' ||
+				str[e - 1] == ')' || str[e - 1] == '{' ||
+				str[e - 1] == '}')
+				str[e] = str[e] - 32;
+		}
+	}
+	return (str);	
+}
