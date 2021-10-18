794b5e6fce18a79c7160a9d57f3b2177c89e9a19
AngelPiva <angelpiva6@gmail.com>
2021-10-18 10:35:57 -0700

    evenmore pointers C2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*_strchr - function that locates a character in a string
+*@s: string
+*@c: character
+*Return: p
+*/
+
+char *_strchr(char *s, char c)
+{
+	int d;
+	char *p;
+
+	for (d = 0; s[d] != '\0'; d++)
+	{
+	if (s[d] == c)
+	{
+		p = &s[d];
+		break;
+	}
+	else
+		p = '\0';
+	}
+	return (p);
+}
