6b7a01a362baaa2bdd292146892a6d88bfdcc8ba
AngelPiva <angelpiva6@gmail.com>
2021-10-18 05:39:11 -0700

    more pointers C7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*
+*
+*/
+
+char *leet(char *a)
+{
+	char letras[10] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+	char numeros[10] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+	int s = 0, n = 0;
+
+	while (a[s])
+	{
+		n = 0;
+		while (n < 10)
+		{
+			if (a[s] == letras[n])
+				a[s] = numeros[n];
+			n++;
+		}
+	s++;
+	}
+	return (a);
+}
