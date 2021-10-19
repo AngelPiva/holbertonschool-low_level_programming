f47e46a0f5e7ac23e005067186002422aa0e2781
AngelPiva <angelpiva6@gmail.com>
2021-10-19 11:01:10 -0700

    evenmore pointers C5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
-*
-*
+*_strstr - function that locates a substring
+*@needle: finds the first occurrence of the substring
+*@haystack: int this string
+*Return:  a pointer to the beginning of the located substring
 */
 
 char *_strstr(char *haystack, char *needle)
