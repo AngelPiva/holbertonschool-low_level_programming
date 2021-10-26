5dc946f57d128d2b103fcd5b607e1e5cc5f7e4f2
AngelPiva <angelpiva6@gmail.com>
2021-10-26 09:40:41 -0700

    malloc C1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,12 +12,14 @@ char *_strdup(char *str)
 	char *n;
 	int a, c;
 
-	if (str == NULL)
-		return (NULL);
 	for (c = 0; str[c] != 0; c++)
 	{
 	}
+	if (str == NULL)
+		return (NULL);
 	n = malloc(c * sizeof(char));
+	if (n == 0)
+		return (NULL);
 	for (a = 0; a < c; a++)
 		n[a] = str[a];
 	return (n);
