8424c1923875151c758988faa571c5b63cba7653
AngelPiva <angelpiva6@gmail.com>
2021-10-28 07:10:03 -0700

    more malloc C0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,16 @@
 #include <stdlib.h>
 
 /**
-*
-*
-*
+*malloc_checked -  function that allocates memory using malloc
+*@b: b
+*Return: ptr
 */
 
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
-	ptr = (unsigned int *) malloc(b * sizeof(unsigned int));
+	ptr = (unsigned int *)malloc(b * sizeof(unsigned int));
 	if (ptr == NULL)
 		exit(98);
 	return (ptr);
