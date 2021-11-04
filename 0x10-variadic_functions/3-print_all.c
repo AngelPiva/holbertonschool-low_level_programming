0d31c38ef0d2be84d9efa12dcce7f25bf5b2ab2c
AngelPiva <angelpiva6@gmail.com>
2021-11-04 15:57:51 -0700

    variadic functions c3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+*print_all - function that prints anything
+*/
+
+void print_all(const char * const format, ...)
+{
+
+	printf("\n");
+}
