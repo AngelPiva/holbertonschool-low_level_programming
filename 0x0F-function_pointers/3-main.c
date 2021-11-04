3c21a4a27af5c2c79c2527571c49fed108547d22
AngelPiva <angelpiva6@gmail.com>
2021-11-03 18:43:11 -0700

    function pointers C3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+*main - entry point
+*@argc: argument count
+*@argv: argument value
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int (*operator)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	operator = get_op_func(argv[2]);
+
+	if (!operator)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", operator(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
