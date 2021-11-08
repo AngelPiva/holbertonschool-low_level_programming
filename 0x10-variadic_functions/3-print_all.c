333f029fe302626150f0068ca329decc25dd3e79
AngelPiva <angelpiva6@gmail.com>
2021-11-08 12:16:24 -0800

    variadic functions C3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,13 +2,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <stddef.h>
+#include <string.h>
 
 /**
 *print_all - function that prints anything
+*@format: list of types of arguments passed to the function
 */
 
 void print_all(const char * const format, ...)
 {
+	va_list pr;
+	pr_a print[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	int i = 0;
+	int p = 0;
+	char *separator = "";
 
+	va_start(pr, format);
+
+	while (format[i])
+	{
+		p = 0;
+		while (print[p].printer != NULL)
+		{
+			if (print[p].printer[0] == format[i])
+			{
+				printf("%s", separator);
+				print[p].f(pr);
+				separator = ", ";
+			}
+			p++;
+		}
+		i++;
+	}
+	va_end(pr);
 	printf("\n");
+}
+
+/**
+*print_char - prints a char
+*@ap: list
+*/
+
+void print_char(va_list ap)
+{
+	printf("%c", va_arg(ap, int));
+}
+
+/**
+*print_int - prints a integer
+*@ap: list
+*/
+
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
+
+/**
+*print_float - prints a float
+*@ap: list
+*/
+
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+*print_string - prints a string
+*@ap: list
+*/
+
+void print_string(va_list ap)
+{
+	printf("%s", va_arg(ap, char *));
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,28 @@
 #ifndef HEADER
 #define HEADER
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+*struct op_s - option structure
+*@printer: format
+*@f: function asociated
+*/
+typedef struct op_s
+{
+	char *printer;
+	void (*f)(va_list pr);
+} pr_a;
+
+void print_char(va_list pr);
+void print_int(va_list pr);
+void print_float(va_list pr);
+void print_string(va_list pr);
 #endif
