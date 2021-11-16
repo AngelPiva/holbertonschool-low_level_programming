9a4a53bf963fa6d609962f0a464020ebc0b87088
AngelPiva <angelpiva6@gmail.com>
2021-11-16 06:49:25 -0800

    more singly linked lists C9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *insert_nodeint_at_index -  function that inserts a new node at a given position
+ *@head: head
+ *@idx: given position
+ *
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *tmp;
+	unsigned int count = 0;
+
+	if (head)
+	{
+		new = malloc(sizeof(listint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		if (idx > 0)
+		{
+			tmp = *head;
+			while (tmp)
+			{
+				if (idx == count)
+				{
+					new->next = tmp->next;
+					tmp->next = new;
+					return (new);
+				}
+				count++;
+				tmp = tmp->next;
+			}
+		}
+		else
+		{
+			new->next = *head;
+			*head = new;
+		}
+		return (new);
+	}
+	return (NULL);
+}
