2e791fae05a57b10b364fc81981956a0df5f1fc6
AngelPiva <angelpiva6@gmail.com>
2021-11-15 19:25:32 -0800

    more singly linked lists C7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *
+ *
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int x = 0;
+
+	if (head)
+	{
+	while (head != NULL)
+	{
+		if (index == x)
+		{
+			return (head);
+		}
+		x++;
+		head = head->next;
+	}
+	}
+	return (NULL);
+}
