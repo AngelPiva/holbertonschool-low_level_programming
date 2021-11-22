5467ef3d7f028b4f7afc2d3996e0942af843e134
AngelPiva <angelpiva6@gmail.com>
2021-11-22 11:33:03 -0800

    files I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, count = _strlen(text_content);
+	int fd, w, count = _strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
@@ -17,7 +17,11 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content)
-		write(fd, text_content, count);
+	{
+		w = write(fd, text_content, count);
+		if (w != count)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
