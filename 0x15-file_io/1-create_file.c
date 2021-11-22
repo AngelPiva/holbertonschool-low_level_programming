47d20942ee6244f6e73d3e8cd5299de6e9a1f78c
AngelPiva <angelpiva6@gmail.com>
2021-11-22 11:29:17 -0800

    files I/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *create_file - function that creates a file.
+ *@filename: name of the file to create
+ *@text_content: NULL terminated string to write to the file
+ *Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, count = _strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+		write(fd, text_content, count);
+	close(fd);
+	return (1);
+}
+
+/**
+*_strlen - returns the length of a string
+*@s: pointer
+*Return: 0
+*/
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	c++;
+	return (c);
+	return (0);
+}
