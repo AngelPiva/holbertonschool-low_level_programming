493fd277c8f968251e3ed36d6e6ec39bad12e06f
AngelPiva <angelpiva6@gmail.com>
2021-11-22 09:26:40 -0800

    files I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,14 +9,14 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char fd, r;
-	char *buf = malloc(sizeof(char *) * letters);
+	int fd, r;
+	char *buf = malloc((sizeof(char *)) * letters);
 
-	if (filename == NULL)
-		return (0);
 	if (letters <= 0)
 		return (0);
-	fd = open(filename, O_RDONLY, 0600);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY, 600);
 	if (fd == -1)
 		return (0);
 	r = read(fd, buf, letters);
