646e41a7a242cdd89a60cc4d8b01dd55c231164e
AngelPiva <angelpiva6@gmail.com>
2021-11-22 11:48:17 -0800

    files I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (text_content)
-		write(fd, text_content, _strlen(text_content));
+		write(STDIN_FILENO, text_content, _strlen(text_content));
 	close(fd);
 	return (1);
 }
