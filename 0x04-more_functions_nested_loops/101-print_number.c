a4ab2e30221d563b534876bf759abcd9584e11ee
AngelPiva <angelpiva6@gmail.com>
2021-11-28 13:38:30 -0800

    print number C

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_number - function that prints an integer
+ *@n: integer
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	if (n / 10)
+		print_number(n / 10);
+	_putchar(n % 10 + 48);
+}
