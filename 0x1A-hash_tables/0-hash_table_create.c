f99f00145d499cf39eb87a54e6ca43f87b0a5724
AngelPiva <angelpiva6@gmail.com>
2022-01-28 07:28:23 -0800

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,14 +9,16 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_hash = NULL;
+	hash_table_t *new_hash_table = NULL;
+	hash_node_t **array = NULL;
 
-	new_hash = malloc(sizeof(hash_table_t));
-	if (!new_hash)
+	new_hash_table = malloc(sizeof(hash_table_t));
+	if (!new_hash_table)
 		return (NULL);
-	new_hash = malloc(sizeof(hash_node_t *) * size);
-	if (!new_hash)
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (!array)
 		return (NULL);
-	new_hash->size = size;
-	return (new_hash);
+	new_hash_table->array = array;
+	new_hash_table->size = size;
+	return (new_hash_table);
 }
