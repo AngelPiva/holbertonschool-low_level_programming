f81a86d710c5876df1d80584c8cb0cac4762adfb
AngelPiva <angelpiva6@gmail.com>
2022-01-28 12:11:10 -0800

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ *hash_table_delete - deletes a hash table
+ *@ht: hash table
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int n = 0;
+	hash_node_t *tmp = NULL, *aux = NULL;
+
+	if (!ht)
+		return;
+
+	while (n < ht->size)
+	{
+		tmp = ht->array[n];
+		if (tmp)
+		{
+			while (tmp)
+			{
+				aux = tmp->next;
+				free(tmp->key);
+				free(tmp->value);
+				free(tmp);
+				tmp = aux;
+			}
+		}
+		n++;
+	}
+	free(ht->array);
+	free(ht);
+}
