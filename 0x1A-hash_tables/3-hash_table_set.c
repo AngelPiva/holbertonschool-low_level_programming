487e03a374650a4c33a75bd86787c179a52505f8
AngelPiva <angelpiva6@gmail.com>
2022-01-28 12:16:14 -0800

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((unsigned char *) key, ht->size);
 	tmp = ht->array[index];
-	if (tmp && tmp->key == key)
+	if (tmp && strcmp(tmp->key, key) == 0)
 	{
 		free(tmp->value);
 		tmp->value = strdup(value);
