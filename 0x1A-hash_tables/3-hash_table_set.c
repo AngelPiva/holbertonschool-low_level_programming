0aa9b9b27bc5e16d1432aa777ba4f1da264ce548
AngelPiva <angelpiva6@gmail.com>
2022-01-28 09:17:18 -0800

    task 43

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new = NULL, *tmp = NULL;
 	unsigned long int index = 0;
 
-	if (!ht || !key || strcmp(key, "") == 0)
+	if (!ht || !key)
 		return (0);
 	index = key_index((unsigned char *) key, ht->size);
 	tmp = ht->array[index];
